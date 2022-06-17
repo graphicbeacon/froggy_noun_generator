716cc481a01ba34b631f667dfa103b25d0f48154
Jermaine Oppong <jermaine@creativebracket.com>
2022-06-17 20:27:09 +0100

    Update tests

--- test/routes/noun/generate_test.dart
+++ test/routes/noun/generate_test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
-import 'package:english_words/english_words.dart';
 import 'package:froggy/language_service.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -14,7 +13,7 @@ class _MockLanguageService extends Mock implements LanguageService {}
 
 void main() {
   group('GET /noun/generate', () {
-    test('response with generated nouns by count', () {
+    test('responds with generated nouns by count', () {
       final context = _MockRequestContext();
       final service = _MockLanguageService();
       final request =
@@ -31,5 +30,42 @@ void main() {
       expect(response.statusCode, HttpStatus.ok);
       expect(response.body(), completion(equals('apple banana')));
     });
+
+    test('responds with 400 if count is not a number', () {
+      final context = _MockRequestContext();
+      final service = _MockLanguageService();
+      final request = Request.get(
+        Uri.parse('http://localhost:8080/noun/generate?count=NaN'),
+      );
+
+      when(() => context.request).thenReturn(request);
+      when(() => context.read<LanguageService>()).thenReturn(service);
+
+      final response = route.onRequest(context);
+
+      expect(response.statusCode, HttpStatus.badRequest);
+      expect(
+        response.body(),
+        completion(
+          equals('Query parameter `count` is not a number'),
+        ),
+      );
+    });
+
+    // TODO: Up for a challenge? Add more tests
+
+    // TODO: responds with 400 if length is not a number
+
+    // TODO: responds with 400 if syllableLength is not a number
+
+    // TODO: responds with results containing firstLetter matches
+
+    // TODO: responds with results containing lastLetter matches
+
+    // TODO: responds with results containing length matches
+
+    // TODO: responds with results containing syllableLength matches
+
+    // TODO: responds with results containing comparison match
   });
 }
